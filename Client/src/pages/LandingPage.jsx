8f1841ef4c3629832e6deac71d5ab1edffc1536c
githinjiEssy <esthergithinji49@gmail.com>
2026-03-07 16:54:47 +0300

    landing page

---
+++ Client/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <div>
+        <h1>Welcome to the Medication Tracker App</h1>
+    </div>
+  )
+}
+
+export default LandingPage
