d06eb1f407c37fdda8ba33179f76c049184f628d
MuthonduG <muthondugithinji@gmail.com>
2026-03-08 18:54:55 +0300

    initial commit for backend app

--- server/medical_tracking/urls.py
+++ server/medical_tracking/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for thegram project.
+URL configuration for medical_tracking project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
--- server/medical_tracking/wsgi.py
+++ server/medical_tracking/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for thegram project.
+WSGI config for medical_tracking project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'thegram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'medical_tracking.settings')
 
 application = get_wsgi_application()
