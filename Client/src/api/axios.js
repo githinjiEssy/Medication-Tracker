3d8c6efb8dbafd9c9bfe1c730d2ee7092ca03ec7
joymakesyouhappy <128835948+joymakesyouhappy@users.noreply.github.com>
2026-03-19 15:18:45 +0300

    fix- updated base URL in axios to resolve the invalid login bug

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/oauth/', // Django URL
+    baseURL: 'http://127.0.0.1:8000/api/auth/', // Django URL
     headers: {
         'Content-Type': 'application/json',
     }
